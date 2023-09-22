734d20d66d9a6244e9208fa489faa4451441bad8
vani <vani.suw@gmail.com>
2023-09-22 09:55:46 +0700

    VariabelCoffeShop

---
+++ PelayanannCoffeeShop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class PelayanannCoffeeShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss: ");
+        LocalTime localTime = LocalTime.now();
+
+        // VARIABEL DAN TIPE DATA
+        String nama_pelanggan, nomor_kontak, status_reservasi, nama_pegawai, metide_pembayaran, catatan;
+        int nomor_meja, jumlah_tamu, kapasitas, total_harga;
+        DateTimeFormatter tanggal, waktu;
+        boolean konfirmasi;
+
+
+
+
+    }
+}
